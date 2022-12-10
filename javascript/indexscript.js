bd93da837119d54dc95609f0e10e7b8dd06bf042
Iellen Luiza Lino Mendes <105090253+iellenLuiza@users.noreply.github.com>
2022-12-10 20:44:58 -0300

    Comecei a estrutura da página inicial

---
+++ javascript/indexscript.js
@@ -0,0 +1,17 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+/* Ativa o negócio de passar as imagens do carrossel de 5 em 5 segundo */
+setInterval( function(){
+    nextImag();
+}, 5000)
+
+/* Passa as imagens do carrossel*/
+function nextImag(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
