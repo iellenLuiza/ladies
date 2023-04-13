1e8c01014534010c1634098ed33dfb367f14c266
Iellen Luiza Lino Mendes <105090253+iellenLuiza@users.noreply.github.com>
2023-04-13 16:15:22 -0300

    ação de fixar interações [obs: ainda com bug]

--- assets/javascript/telaPublicacao.js
+++ assets/javascript/telaPublicacao.js
@@ -1,3 +1,19 @@
 /* ---------- FIXAR INTERAÇÕES LATERAIS ---------- */
+const inter=document.getElementById("interacoes");
+const refB = document.getElementsByClassName("comentario-vejaMais")[0];
+const ref=document.getElementsByClassName("conteiner")[0];
+const alturaRef=ref.offsetTop;
+const alturaRefB=refB.offsetTop;
 
+window.onscroll=function(){
+    fixarInter();
+}
+// função de fixar muda alguns critérios para fixar o cabeçalho
+function fixarInter(){
+    if(window.pageYOffset >= alturaRef){
+        inter.classList.add("fixarInter");
+    }else{
+        inter.classList.remove("fixarInter");
+    }
+} //Tem que ajustar para quando chegar no fim do texto "subir" dnv
 /* ------------------------------------- */
