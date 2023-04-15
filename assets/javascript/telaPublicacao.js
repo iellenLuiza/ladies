20005e40aa2d56de8c369611dab15f0576ff09ec
Iellen Luiza Lino Mendes <105090253+iellenLuiza@users.noreply.github.com>
2023-04-15 19:47:56 -0300

    função interações sumir e aparecer
    consertei o problemas das interações terem que aparecer ou sumir dependendo da altura mas na base da gambiarra. O js do cabeçalho não estava funcionando na tela de publicações então por enquanto eu copiei o dógigo e coloquei no js da tela de publicações

--- assets/javascript/telaPublicacao.js
+++ assets/javascript/telaPublicacao.js
@@ -1,19 +1,55 @@
 /* ---------- FIXAR INTERAÇÕES LATERAIS ---------- */
-const inter=document.getElementById("interacoes");
-const refB = document.getElementsByClassName("comentario-vejaMais")[0];
-const ref=document.getElementsByClassName("conteiner")[0];
-const alturaRef=ref.offsetTop;
-const alturaRefB=refB.offsetTop;
+var inter=document.getElementById("interacoes");
+var ref=document.getElementsByClassName("conteiner")[0];
+var refB=document.getElementById("idAutor");
+var alturaRef=ref.offsetTop;
+var alturaRefB=refB.offsetTop;
 
 window.onscroll=function(){
     fixarInter();
+    fixarNoTopo();
 }
-// função de fixar muda alguns critérios para fixar o cabeçalho
 function fixarInter(){
-    if(window.pageYOffset >= alturaRef){
+    if( window.pageYOffset >= alturaRef && window.pageYOffset <= alturaRefB-590){
         inter.classList.add("fixarInter");
-    }else{
+    } else{
+        inter.style.transition = "0.3s linear";
         inter.classList.remove("fixarInter");
     }
 } //Tem que ajustar para quando chegar no fim do texto "subir" dnv
+/* ------------------------------------- */
+
+
+
+/* ---------- FIXAR CABEÇALHO ---------- */
+const header=document.getElementsByTagName("header")[0];// Pega o header
+const nav=document.getElementsByTagName("nav")[0];//Pega a nav 
+const alturaTopo=nav.offsetTop;//Usa o topo do elemento nav como critério de altura para chamar a função de fixar
+// chama função de fixar ao rolar a tela
+// função de fixar muda alguns critérios para fixar o cabeçalho
+function fixarNoTopo(){
+    if(window.pageYOffset >= alturaTopo){
+        header.classList.add("fixarTopo");
+        document.querySelector('.logo-nav').style.padding = '0';
+        document.querySelector('.logo').style.transform = 'scale(0.75)';
+        document.querySelector('nav').style.transform = 'scale(0.9)';
+        document.querySelector('nav').style.marginTop = 'auto';
+        document.querySelector('nav').style.marginBottom = 'auto';
+        document.querySelector('nav').style.marginRight = 'auto';
+        document.querySelector('nav').style.marginLeft = '-6vh';
+        document.querySelector('.faixa').style.marginTop = '-1.7vh';
+        document.querySelector('.faixa').style.height = '1.4vh';
+    }else{
+        header.classList.remove("fixarTopo");
+        document.querySelector('.logo-nav').style.padding = '1.1vw';
+        document.querySelector('.logo').style.transform = 'scale(1)';
+        document.querySelector('nav').style.transform = 'scale(1)';
+        document.querySelector('nav').style.marginTop = 'auto';
+        document.querySelector('nav').style.marginBottom = '0';
+        document.querySelector('nav').style.marginRight = '0';
+        document.querySelector('nav').style.marginLeft = 'auto';
+        document.querySelector('.faixa').style.height = '3vh';
+        document.querySelector('.faixa').style.marginTop = '-0.4vh';
+    }
+}
 /* ------------------------------------- */
